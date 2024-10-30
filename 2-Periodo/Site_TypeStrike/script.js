e6ddc032dc12339bb676b02592da56d8e1a12375
Júlio Gabriel <juliogabriel10@icloud.com>
2024-10-30 13:08:20 -0300

    alinhamento
    Alinhamento dos tópicos

--- 2-Periodo/Site_TypeStrike/script.js
+++ 2-Periodo/Site_TypeStrike/script.js
@@ -7,3 +7,20 @@ function closeMenu() {
   const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show");
 }
+
+function scrollToSection(event, sectionId) {
+  event.preventDefault(); // Previne o comportamento padrão do link
+  const section = document.getElementById(sectionId);
+  
+  // Ajuste do scroll considerando o cabeçalho fixo
+  const headerOffset = document.querySelector('header').offsetHeight;
+  const elementPosition = section.getBoundingClientRect().top;
+  const offsetPosition = elementPosition + window.scrollY - headerOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: 'smooth' // Para uma rolagem suave
+  });
+
+  closeMenu(); // Fecha o menu após a seleção
+}

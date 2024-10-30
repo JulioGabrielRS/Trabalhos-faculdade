ef242bd7930f45dab7f5e5ebdb68be3d4cc2951a
Júlio Gabriel <juliogabriel10@icloud.com>
2024-10-30 11:50:21 -0300

    Menu
    Adicionei um menu e deixei o site responsivo

---
+++ 2-Periodo/Site_TypeStrike/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.toggle("show");
+}
+
+function closeMenu() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}

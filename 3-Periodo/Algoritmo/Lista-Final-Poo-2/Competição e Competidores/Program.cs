5ec25f954c7a71e72c62ad44e6ed1e131045348d
Júlio Gabriel <140089199+JulioGabrielRS@users.noreply.github.com>
2025-06-25 15:15:51 -0300

    trabalho-poo

---
+++ 3-Periodo/Algoritmo/Lista-Final-Poo-2/Competição e Competidores/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Competidor
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Modalidade { get; set; }
+}
+
+class Competicao
+{
+    public string Nome { get; set; }
+    public List<Competidor> Competidores { get; } = new List<Competidor>();
+
+    public void AdicionarCompetidor(Competidor competidor)
+    {
+        Competidores.Add(competidor);
+    }
+}
+
+class Program
+{
+    static Competicao competicao = new Competicao();
+
+    static void Main()
+    {
+        bool executando = true;
+
+        while (executando)
+        {
+            Console.WriteLine("\n--- MENU ---");
+            Console.WriteLine("1. Cadastrar Competição");
+            Console.WriteLine("2. Adicionar Competidor");
+            Console.WriteLine("3. Listar Competidores");
+            Console.WriteLine("4. Sair");
+            Console.Write("Opção: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    CadastrarCompeticao();
+                    break;
+                case "2":
+                    AdicionarCompetidor();
+                    break;
+                case "3":
+                    ListarCompetidores();
+                    break;
+                case "4":
+                    executando = false;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+    }
+
+    static void CadastrarCompeticao()
+    {
+        Console.Write("Nome da Competição: ");
+        competicao.Nome = Console.ReadLine();
+        Console.WriteLine("Competição cadastrada!");
+    }
+
+    static void AdicionarCompetidor()
+    {
+        if (string.IsNullOrEmpty(competicao.Nome))
+        {
+            Console.WriteLine("Cadastre uma competição primeiro!");
+            return;
+        }
+
+        var competidor = new Competidor();
+
+        Console.Write("Nome: ");
+        competidor.Nome = Console.ReadLine();
+
+        Console.Write("Idade: ");
+        competidor.Idade = int.Parse(Console.ReadLine());
+
+        Console.Write("Modalidade: ");
+        competidor.Modalidade = Console.ReadLine();
+
+        competicao.AdicionarCompetidor(competidor);
+        Console.WriteLine("Competidor adicionado!");
+    }
+
+    static void ListarCompetidores()
+    {
+        if (competicao.Competidores.Count == 0)
+        {
+            Console.WriteLine("Nenhum competidor cadastrado!");
+            return;
+        }
+
+        Console.WriteLine($"\nCompetição: {competicao.Nome}");
+        foreach (var c in competicao.Competidores)
+        {
+            Console.WriteLine($"Nome: {c.Nome} | Idade: {c.Idade} | Modalidade: {c.Modalidade}");
+        }
+    }
+}
---
+++ 3-Periodo/Algoritmo/Lista-Final-Poo-2/Tratamento de Exceções/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Digite o primeiro número: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o segundo número: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Resultado: {num1 / num2}");
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro: Valor inválido. Digite um número inteiro.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Erro: Não é possível dividir por zero.");
+            }
+        }
+    }
+}
